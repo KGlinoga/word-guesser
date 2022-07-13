339acc509810e0a9c96a794364165b8baf2fb87b
Kristina Faye <kglinoga@gmail.com>
2022-07-12 19:04:19 -0700

    Training WheelsInitiated

--- assets/script.js
+++ assets/script.js
@@ -1,32 +1,41 @@
-var timeEl = document.querySelector(".time");
-var mainEl = document.getElementById("main");
-var textAreaEl = document.querySelector('#textarea')
 
-var secondsLeft = 10;
 
-mainEl.addEventListener('click', function(event) {
-    event.preventDefault();
-    setTime();
-})
 
-function setTime() {
-    // Sets interval in variable
-    var timerInterval = setInterval(function() {
-      secondsLeft--;
-      timeEl.textContent = secondsLeft + " seconds left to guess!";
+
+
+
+
+
+
+
+// var timeEl = document.querySelector(".time");
+// var mainEl = document.getElementById("main");
+// var textAreaEl = document.querySelector('#textarea')
+
+// var secondsLeft;
+
+// mainEl.addEventListener('click', function(event) {
+//     event.preventDefault();
+//     secondsLeft = 10;
+//     setTime();
+// })
+
+// function setTime() {
+   
+//     var timerInterval = setInterval(function() {
+//       secondsLeft--;
+//       timeEl.textContent = secondsLeft + " seconds left to guess!";
   
-      if(secondsLeft === 0) {
-        // Stops execution of action at set interval
-        clearInterval(timerInterval);
-        // Calls function to create and append image
-        sendMessage("Time's Up!");
-      }
+//       if(secondsLeft === 0) {
+//         clearInterval(timerInterval);
+//         sendMessage("Time's Up!");
+//       }
   
-    }, 1000);
-  }
+//     }, 1000);
+//   }
 
-  function sendMessage() {
-    alert("Bummer!");
-  };
+//   function sendMessage() {
+//     alert("Bummer!");
+//   };
 
  
